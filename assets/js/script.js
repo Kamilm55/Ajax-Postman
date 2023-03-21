ffb1b78e497af0ec9a497291f4b07bebf99294a1
unknown <abulfazfa@code.edu.az>
2023-03-21 23:32:49 +0400

    LivePrayertime

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,45 +10,76 @@ $(document).ready(function () {
         method: "get",
         success: function (data) {
             data.data.forEach(element => {
-                $(".todayDate").text(`${localday}/${localmonth}/${localyear}`)
+                $(".todayDate").text(`${Number(localday) + 1}/${localmonth}/${localyear}`)
                 $("h3").text(`Prayer Times in ${timezone[1]}`);
-                    if (element.date.readable.slice(0, 2) == localday) {
+                if (element.date.readable.slice(0, 2) == localday) {
 
-                        $(".fdate").text(element.timings.Fajr.slice(0, 6));
-                        $(".sdate").text(element.timings.Sunrise.slice(0, 6));
-                        $(".ddate").text(element.timings.Dhuhr.slice(0, 6));
-                        $(".adate").text(element.timings.Asr.slice(0, 6));
-                        $(".mdate").text(element.timings.Maghrib.slice(0, 6));
-                        $(".idate").text(element.timings.Isha.slice(0, 6));
-                      
-                    }
-                   
-                    
-                console.log();
+                    $(".fdate").text(element.timings.Fajr.slice(0, 6));
+                    $(".sdate").text(element.timings.Sunrise.slice(0, 6));
+                    $(".ddate").text(element.timings.Dhuhr.slice(0, 6));
+                    $(".adate").text(element.timings.Asr.slice(0, 6));
+                    $(".mdate").text(element.timings.Maghrib.slice(0, 6));
+                    $(".idate").text(element.timings.Isha.slice(0, 6));
 
 
-                    var countDownDate = element.timings.Asr;
-
-
-                    var x = setInterval(function () {
-                
-                        var now = new Date().getTime();
-                
-                        var distance = countDownDate - now;
-                
-                        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                
-                        document.getElementById("neqederqalib").innerHTML = minutes;
+                    if ((new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Isha.slice(0, 5)}:00`)) - new Date() < 0) {
+                        var countDownDate = new Date(`${localmonth}/${Number(localday) + 1}/${localyear} ${element.timings.Fajr.slice(0, 5)}:00`);
                         console.log(countDownDate);
-                
-                        
-                    }, 1000);
+                        $("#prayerTimeName").text('Fajr');
+                        Counter(countDownDate);
+                    }
+                    else if ((new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Maghrib.slice(0, 5)}:00`)) - new Date() < 0){
+                        var countDownDate = new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Isha.slice(0, 5)}:00`);
+                        console.log(countDownDate);
+                        $("#prayerTimeName").text('Isha');
+                        Counter(countDownDate);
+                    }
+                    else if ((new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Asr.slice(0, 5)}:00`)) - new Date() < 0){
+                        var countDownDate = new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Maghrib.slice(0, 5)}:00`);
+                        console.log(countDownDate);
+                        $("#prayerTimeName").text('Maghrib');
+                        Counter(countDownDate);
+                    }
+                    else if ((new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Dhuhr.slice(0, 5)}:00`)) - new Date() < 0){
+                        var countDownDate = new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Asr.slice(0, 5)}:00`);
+                        console.log(countDownDate);
+                        $("#prayerTimeName").text('Asr');
+                        Counter(countDownDate);
+                    }
+                    else if ((new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Fajr.slice(0, 5)}:00`)) - new Date() < 0){
+                        var countDownDate = new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Dhuhr.slice(0, 5)}:00`);
+                        console.log(countDownDate);
+                        $("#prayerTimeName").text('Dhuhr');
+                        Counter(countDownDate);
+                    }
+                    else{
+                        var countDownDate = new Date(`${localmonth}/${Number(localday)}/${localyear} ${element.timings.Fajr.slice(0, 5)}:00`);
+                        console.log(countDownDate);
+                        $("#prayerTimeName").text('Fajr');
+                        Counter(countDownDate);
+                    }
 
 
 
+
+                        function Counter() {
+                            var x = setInterval(function () {
+
+                                var now = new Date().getTime();
+                                var distance = countDownDate - now;
+
+                                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                                document.getElementById("neqederqalib").innerHTML = (hours + " " + minutes + " " + seconds);
+                                
+
+                            }, 1000);
+                        }
+
+                }
 
 
 
@@ -79,7 +110,7 @@ $(document).ready(function () {
 
     $("button").click(function (e) {
         e.preventDefault();
-        let month = (new Date($("#date").val()).getMonth())+ 1;
+        let month = (new Date($("#date").val()).getMonth()) + 1;
         let year = new Date($("#date").val()).getFullYear();
         let city = $("#city").val();
         $.ajax({
@@ -90,10 +121,10 @@ $(document).ready(function () {
                 localTime(city);
                 $("h3").text(`Prayer Times in ${city.charAt(0).toUpperCase() + city.slice(1)}`);
                 data.data.forEach(element => {
-                    
+
 
                     if (element.date.readable.slice(0, 2) == localday) {
-                        
+
                         $(".fdate").text(element.timings.Fajr.slice(0, 6));
                         $(".sdate").text(element.timings.Sunrise.slice(0, 6));
                         $(".ddate").text(element.timings.Dhuhr.slice(0, 6));
@@ -101,10 +132,10 @@ $(document).ready(function () {
                         $(".mdate").text(element.timings.Maghrib.slice(0, 6));
                         $(".idate").text(element.timings.Isha.slice(0, 6));
                     }
-                    
-                        
-                       
-                    
+
+
+
+
                     let tr =
                         `
                         <tr>
@@ -125,15 +156,15 @@ $(document).ready(function () {
         });
 
     })
-    
-    function localTime(city){
+
+    function localTime(city) {
         $.ajax({
             url: `https://api.aladhan.com/v1/calendarByAddress/${localyear}/${localmonth}?address=${city}&method=2`,
             method: "get",
             success: function (data) {
                 data.data.forEach(element => {
-                    
-                    
+
+
                     if (element.date.readable.slice(0, 2) == localday) {
 
                         $(".fdate").text(element.timings.Fajr.slice(0, 6));
@@ -144,15 +175,15 @@ $(document).ready(function () {
                         $(".idate").text(element.timings.Isha.slice(0, 6));
                     }
 
-                    
-    
-    
+
+
+
                 });
-    
+
             }
         });
     }
-    
+
 
 
 });
